920423300584eeca7813835ae97d727b9556c3d6
Jimmy Bull <jbull635@gmail.com>
2021-06-29 17:16:40 +0200

    ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p>ok jimmy</p>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
