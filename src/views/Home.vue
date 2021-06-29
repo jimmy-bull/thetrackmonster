ec0c4d6ddb40320885271b516c86dd6dc5dc4517
Jimmy Bull <jbull635@gmail.com>
2021-06-29 17:18:29 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <p>ok jimmy</p>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
