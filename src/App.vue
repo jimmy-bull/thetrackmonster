14664e155514196c4ed2798b1bb5c5f15c8fb2ec
Jimmy Bull <jbull635@gmail.com>
2021-06-29 13:44:52 +0200

    add encore

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
   <router-view/>
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
 
 <script>
