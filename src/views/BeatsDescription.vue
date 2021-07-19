c28a24c0057f3678ab577d00398de7f94f512688
TheManous <osman2trasco@gmail.com>
2021-07-19 21:11:53 +0200

    Update BeatsDescription.vue

--- src/views/BeatsDescription.vue
+++ src/views/BeatsDescription.vue
@@ -1,552 +1,233 @@
 <template>
-  <div class="flex_container">
-    <div class="div_songCover_infos">
-      <div class="cover"></div>
-      <!-- Div Informations sur le Song-->
-      <div class="infos_song">
-        <!-- Div Titre son -->
-        <div class="div_songTitle_play-boutton">
-          <unicon
-            name="play"
-            id="play-pause"
-            width="30"
-            height="30"
-            fill="#42b983"
-          />
-          <span class="Title"> Warrior </span>
-        </div>
-        <!-- End div Titre son -->
+     <!-- DIV COVER AND SONG INFOS -->
+        <div class="Lil_div_cover_song_info">
 
-        <!-- Div nom artiste -->
-        <div class="div_nom_artiste">
-          <span class="artiste_name"> Pluto </span>
-        </div>
-        <!-- end div nom artiste -->
+            <!-- Div SONG DETAILS AND TAG-->
+            <div class="infos_song">
+                
 
-        <!-- Div nom BPM Date -->
-        <div class="div_bpm_date">
-          <div class="little_bpm_div">BPM</div>
-          <span class="bpm_number">126</span>
-          <span class="date">
-            <unicon
-              name="clock"
-              id="play-pause"
-              width="15"
-              height="15"
-              fill="#42b983"
-            />
-            July 6, 2021</span
-          >
-        </div>
-        <!-- end nom BPM Date -->
+                <!-- COVER IMAGE -->
+                <div class="cover"> <img src="images/Playing/drake.jpg"> </div>
 
-        <div class="div_prix_share">
-          <div class="btn_buy_player hide_on_576">
-            <div class="hide_on_576">
-              <div>
-                <unicon
-                  name="shopping-bag"
-                  width="15"
-                  height="15"
-                  fill="white"
-                />
-              </div>
-            </div>
-            <div class="price_txt">
-              <span style="color: white; font-size: 15px">$29.99</span>
-            </div>
-          </div>
-          <div style="cursor: pointer; margin-left: 10px" class="hide_on_576">
-            <unicon name="share-alt" fill="#42b983" />
-          </div>
-        </div>
-      </div>
-      <!-- End div Informations sur le Song-->
+                <div class="div_song_details">
 
-      <!-- SONG TAGS DIV-->
-      <div class="div_song_tags">
-        <div class="tags">#Tag 1</div>
-        <div class="tags">#Tag 2</div>
-        <div class="tags">#Tag 3</div>
-      </div>
-      <!-- End song tags div-->
-    </div>
-  </div>
-  <div style="margin-top: 50px">
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
+                    <!-- Div Titre son -->
+                    <div class="div_songTitle_play-boutton">
+                        <input type="checkbox" id="play-pause" />
+                        <span class="Title"> TITLE </span>
+                    </div>
+                    <!-- End div Titre son -->
+
+                    <!-- Div nom artiste -->
+                    <div class="div_nom_artiste">
+                        <span class="artiste_name"> Pluto </span>
+                    </div>
+                    <!-- end div nom artiste -->
+
+                    <!-- Div nom BPM Date -->
+                    <div class="div_bpm_date">
+                        <div class="little_bpm_div">BPM</div>
+                        <span class="bpm_number">126</span>
+                        <span class="date">July 6, 2021</span>
+                    </div>
+                    <!-- end nom BPM Date -->
+
+                    <div class="div_prix_share">
+                        <div class="prix_share space_prix">
+                            <img href="#" class="icons_price_share"> $29.99
+                        </div>
+
+                        <div class="prix_share">Share</div>
+                    </div>
+
+                </div>
+
             </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
+
+                <!-- SONG TAGS DIV-->
+                <div class="div_song_tags">
+                    <div class="tags"> #Tag 1</div>
+                    <div class="tags"> #Tag 2</div>
+                    <div class="tags"> #Tag 3</div>
+                </div>
+                <!-- End song tags div-->
+
+            <!-- End div SONG DETAILS AND TAG-->
+
         </div>
-      </div>
-    </div>
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
-            </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
-            </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
-            </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
-            </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="play_list_block_playlist">
-      <div class="image_zone">
-        <div>1</div>
-        <div class="image_box">
-          <img
-            src="https://en.pressemag.fr/wp-content/uploads/2021/01/DaBaby-Press-Photo-1_Photo-Credit_Jackie-Dimailig.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <span>Over</span>
-        </div>
-      </div>
-      <div class="minute_box_big">
-        <div class="minute_box">
-          <span> 02:38</span>
-        </div>
-        <div class="bpm_box">
-          <span>79</span>
-        </div>
-        <div class="tag_box">
-          <div class="tags_element"><span>#Dababy</span></div>
-          <div class="tags_element"><span>#lil durk</span></div>
-        </div>
-      </div>
-      <div class="buy_share_block">
-        <div style="cursor: pointer" class="hide_on_576">
-          <unicon name="share-alt" fill="#42b983" />
-        </div>
-        <div class="btn_buy_player hide_on_576">
-          <div class="hide_on_576">
-            <div>
-              <unicon name="shopping-bag" width="15" height="15" fill="white" />
-            </div>
-          </div>
-          <div class="price_txt">
-            <span style="color: white; font-size: 15px">$29.99</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
  
 
 
 <style scoped>
-.filter_zone {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-.filter_zone > div {
-  width: 20%;
-  margin: 20px;
-}
-.select_style {
-  width: 100%;
-}
-@media only screen and (max-width: 768px) {
-  .filter_zone > div {
-    width: 40%;
-    margin: 5px 20px 5px 20px;
-  }
-}
-@media only screen and (max-width: 400px) {
-  .filter_zone > div {
-    width: 40%;
-    margin: 5px 5px 5px 5px;
-  }
-}
-.btn_buy_player {
-  background: #42b983;
-  display: flex;
-  padding: 2px 15px 5px 15px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 3px;
-  cursor: pointer;
-}
-.btn_buy_player > div {
-  margin: 0 2px 0 2px;
-}
-.tags_element > span {
-  font-size: 12px;
-}
-.tags_element {
-  border-style: solid;
-  padding: 0px 5px 2px 5px;
-  border-width: 1px;
-  border-color: #42b983;
-  color: #42b983;
-}
-.tag_box {
-  display: flex;
-  align-items: center;
-}
-.tag_box > div {
-  margin: 0px 3px 0px 3px;
-}
-.play_list_block_playlist {
-  display: flex;
-  justify-content: space-between;
-  cursor: pointer;
-  margin: 0px 50px 0px 50px;
-}
-.image_box img {
-  width: 35px;
-  height: 35px;
-  object-fit: cover;
-  border-radius: 3px;
-}
+ .flex_container {}
 
-.image_zone,
-.minute_box_big,
-.buy_share_block {
-  display: flex;
-  align-items: center;
-  height: 50px;
-}
-.image_zone > div {
-  margin: 20px 5px 0px 5px;
-}
-.minute_box_big > div {
-  margin: 20px 35px 0px 35px;
-}
-.buy_share_block > div {
-  margin: 20px 15px 0px 15px;
-}
-@media only screen and (max-width: 768px) {
-  .tag_box {
-    display: none;
-  }
-}
-@media only screen and (max-width: 600px) {
-  .minute_box_big {
-    display: none;
-  }
-  .play_list_block_playlist {
-    margin: 0px 10px 0px 10px;
-  }
-  .search_input {
-    margin: 50px 10% 0px 10%;
-  }
-}
-@media only screen and (max-width: 360px) {
-  .price_txt {
-    display: none !important;
-  }
-}
+        .Lil_div_cover_song_info {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            flex-wrap: wrap;
+            margin-top: 10vh;
+        }
 
-.flex_container {
-  margin: 20px;
-}
+        .infos_song {
+            display: flex;
+        }
 
-.div_songCover_infos {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+        .infos_song > div { 
+            margin: 0 20px 0 20px;
+        }
 
-.infos_song {
-  display: block;
-  padding: 6px;
-  margin-right: 20%;
-  margin-left: 25px;
-}
 
-.cover {
-  width: 180px;
-  height: 180px;
-  background-image: url("http://image-api.nrj.fr/medias/2018/09/drake_5ba8a8c8494ff.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  border-radius: 4px;
-}
+        .cover > img {
+            height: 190px;
+            width: 190px;
+            object-fit: cover;
 
-.div_songTitle_play-boutton {
-  display: flex;
-  align-items: center;
-  margin-bottom: 11px;
-  cursor: pointer;
-}
+        }
 
-.Title {
-  font-size: 20px;
-  position: relative;
-  top: 5px;
-  font-weight: bold;
-}
+        .div_song_details {
 
-.div_nom_artiste {
-  margin-bottom: 11px;
-}
+        }
 
-.artiste_name {
-  font-weight: bold;
-  margin-bottom: 11px;
-}
+        #play-pause {
+            width: 70px;
+            height: 30px;
+            margin-left: -18px;
+        }
 
-.div_bpm_date {
-  display: flex;
-  margin-bottom: 11px;
-}
+        .div_songTitle_play-boutton {
+            margin-bottom: 11px;
+        }
 
-.little_bpm_div {
-  border: 1px solid white;
-  background-color: black;
-  color: white;
-  border-radius: 3px;
-  font-size: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-left: 3px;
-  padding-right: 3px;
-}
+        .Title {
+            font-size: 30px;
+            font-weight: bold;
+        }
 
-.bpm_number {
-  padding-left: 2px;
-  margin-right: 15px;
-  font-weight: bold;
-}
+        .div_nom_artiste {
+            margin-bottom: 11px;
+        }
 
-.date {
-  font-weight: bold;
-}
 
-.div_prix_share {
-  display: flex;
-}
-.div_prix_share > div {
-  margin-top: 10px;
-}
+        .artiste_name {
+            font-weight: bold;
+            margin-bottom: 11px;
+            font-size: 18px;
+        }
 
-.prix_share {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-  color: white;
-  width: 110px;
-  height: 40px;
-  border-radius: 3px;
-}
+        .div_bpm_date {
+            display: flex;
+            margin-bottom: 11px;
+            font-size: 15px;
+        }
 
-.space_prix {
-  margin-right: 10px;
-}
+        .little_bpm_div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid white;
+            background-color: #42b983;
+            color: white;
+            border-radius: 3px;
+            font-size: 8px;
+            padding-left: 5px;
+            padding-right: 5px;
+        }
 
-.div_song_tags {
-  display: flex;
-}
+        .bpm_number {
+            padding-left: 2px;
+            margin-right: 15px;
+            font-weight: bold;
+        }
 
-.tags {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #42b983;
-  margin-right: 13px;
-  color: #42b983;
-  height: 34px;
-  width: 120px;
-  background-color: white;
+        .date {
+            font-weight: bold;
+        }
 
-  margin-top: 23%;
-}
+        .div_prix_share {
+            display: flex;
+        }
+
+        .prix_share {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #42b983;
+            color: white;
+            width: 125px;
+            height: 45px;
+            border-radius: 3px;
+            transition: transform .2s; /* Animation */
+        }
+
+        .prix_share:hover{
+            transform: scale(1.1);
+        }
+
+
+        .space_prix {
+            margin-right: 10px;
+        }
+
+
+        .div_song_tags {
+            display: flex;
+            margin-top: 78px;
+        }
+
+        .tags {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid black;
+            margin-right: 13px;
+            border-radius: 20px;
+            height: 40px;
+            width: 110px;
+            background-color: white;
+            color: black;
+        }
+
+
+        /* REPONSIVE MEDIA QUERIES */
+        @media only screen and (max-width: 1024px) {
+
+            .infos_song > div {
+                width: 100%;
+            }
+
+            .infos_song {
+                justify-content: center;
+                flex-direction: column;
+            }
+
+            .Lil_div_cover_song_info{
+                justify-content: center;
+                flex-direction: column;
+            }
+
+            .cover img{
+                margin-left: 11%;
+            }
+
+            .div_songTitle_play-boutton{
+                margin-left: 17%;
+            }
+
+            .div_nom_artiste{
+                margin-left: 32%;
+            }
+
+            .div_bpm_date{
+                margin-left: 16%;
+            }
+
+
+
+        }
+
 </style>
 <script>
 export default {
