74138de5233e89981a11e5a931688d628f309649
Jimmy Bull <jbull635@gmail.com>
2021-08-03 23:18:49 +0200

    Ajout

---
+++ src/components/Share.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+<style scoped>
+</style>
+<script>
+export default {
+  name: "Share",
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    open_modal: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
